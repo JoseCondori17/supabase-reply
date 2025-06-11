d52235794920cd07c7efc20a26450c10a855dd89
JoseCondori17 <jose.condori.p@utec.edu.pe>
2025-06-10 23:29:17 -0500

    Adds the new ColumnService class to manage column-related operations.

--- server/catalog/column.py
+++ server/catalog/column.py
@@ -1,9 +1,18 @@
 from dataclasses import dataclass
 
+from server.catalog.service import BaseService
+from server.storage.disk.file_manager import FileManager
+from server.storage.disk.path_builder import PathBuilder
+
+
 @dataclass
-class Column:
+class Column(BaseService):
     att_name        : str
     att_type_id     : int
     att_len         : int
     att_not_null    : bool
     att_has_def     : bool
+    
+class ColumnService(BaseService):
+    def __init__(self, file_manager: FileManager, path_builder: PathBuilder):
+        super().__init__(file_manager, path_builder)
